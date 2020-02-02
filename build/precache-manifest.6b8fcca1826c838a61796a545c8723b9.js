280b89c08a2d5510742225a148060ae839b801df
Laura S <46318664+sinikala@users.noreply.github.com>
2020-02-02 13:47:52 +0200

    build / puhelinluettelo fullstack

---
+++ build/precache-manifest.6b8fcca1826c838a61796a545c8723b9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd88c53e0e9fb8786779518e413454c2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fd84bf69357341f601ef",
+    "url": "/static/css/main.f5250d36.chunk.css"
+  },
+  {
+    "revision": "a8337ebcd53577ec6c50",
+    "url": "/static/js/2.bf323c43.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.bf323c43.chunk.js.LICENSE"
+  },
+  {
+    "revision": "fd84bf69357341f601ef",
+    "url": "/static/js/main.30872781.chunk.js"
+  },
+  {
+    "revision": "cf9fd95ce589cae23005",
+    "url": "/static/js/runtime-main.2e9fb530.js"
+  }
+]);
